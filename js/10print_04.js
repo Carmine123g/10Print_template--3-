577dd9b19358ec717769401511c3079c3b302174
Carminio123g <carmelolamantia1@gmail.com>
2024-05-22 11:41:32 +0200

    fix5

--- js/10print_04.js
+++ js/10print_04.js
@@ -8,7 +8,7 @@ function setup() {
 
 function draw() {
   background(255); // Cancella lo schermo a ogni frame
-  let step = 1; // Dimensioni delle celle
+  let step = 5; // Dimensioni delle celle
   for (let x = 0; x < width; x += step) {
     for (let y = 0; y < height; y += step) {
       let c = lerpColor(baseColor, color(255), random()); // Calcola una sfumatura del colore di base
@@ -21,3 +21,4 @@ function draw() {
 function mousePressed() {
   baseColor = color(random(255), random(255), random(255)); // Cambia il colore di base al click del mouse
 }
+
